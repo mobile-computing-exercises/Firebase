dcddf08306574b2fbd60ed6c4847d93dc1b41aa0
adnan <aboutadnan@gmail.com>
2019-07-09 18:45:58 +0200

    added spinner for uuid selection

--- app/src/main/java/de/uni_s/ipvs/mcl/assignment5/MainActivity.java
+++ app/src/main/java/de/uni_s/ipvs/mcl/assignment5/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothGattCallback;
@@ -18,7 +21,10 @@ import android.bluetooth.le.ScanFilter;
 import android.bluetooth.le.ScanResult;
 import android.bluetooth.le.ScanSettings;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -44,8 +50,10 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
@@ -53,9 +61,12 @@ public class MainActivity extends AppCompatActivity {
     private TextView temperatureText;
     private TextView humidityText;
     private TextView averageText;
+    private TextView sensorText;
     private Spinner locationPicker;
+    private Spinner sensorPicker;
     // Permission code
     private final int REQUEST_ENABLE_BT = Activity.RESULT_OK;
+    private static final int REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS = 99;
     // BT objects
     private BluetoothAdapter bluetoothAdapter;
     private BluetoothLeScanner bluetoothScanner;
@@ -64,6 +75,7 @@ public class MainActivity extends AppCompatActivity {
     // Sensor values
     private float tempValue, humValue;
     private String location = "Stuttgart";
+    private String uuId;
     // Handlers
     private static final long SCAN_DELAY = 5000;
     private Handler userInterfaceUpdateHandler;
@@ -84,6 +96,9 @@ public class MainActivity extends AppCompatActivity {
     // Dateformats
     SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
     SimpleDateFormat timeFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+    //flags
+    boolean isTemperature;
+    boolean isHumidity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -108,6 +123,13 @@ public class MainActivity extends AppCompatActivity {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
         }
+
+        //check permission
+        if (Build.VERSION.SDK_INT >= 23) {
+            // Marshmallow+ Permission APIs
+            permissionCheck();
+        }
+
         // Setup BT
         setUpBluetooth();
 
@@ -118,29 +140,28 @@ public class MainActivity extends AppCompatActivity {
         humSubtree = uuidSubtree.child("00002A6F-0000-1000-8000-00805F9B34FB");
         locationSubtree = mRef.child("location");
 
-        locationPicker.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        setupLocationSpinner();
+
+        setUpUuIdSpinner();
+
+        setUpListeners();
+
+
+
+
+    }
+
+    private void setUpListeners() {
+
+        uuidSubtree.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Log.i("PICKER", "ITEM SELECTED: " + parent.getSelectedItem().toString());
-                location = parent.getSelectedItem().toString();
-
-                locationSubtree.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        String outputText = getAvarageForLocation(dataSnapshot);
-                        averageText.setText(outputText);
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError error) {
-                        // Failed to read value
-                        Log.w("Database", "Failed to read value.", error.toException());
-                    }
-                });
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                updateUuidPicker(dataSnapshot);
             }
 
             @Override
-            public void onNothingSelected(AdapterView<?> parent) {
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
 
@@ -150,6 +171,9 @@ public class MainActivity extends AppCompatActivity {
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 String outputText = getLastUpdate(dataSnapshot, "temp");
                 temperatureText.setText(outputText);
+                if(isTemperature)
+                    sensorText.setText(getLatestUpdate(dataSnapshot, "temp"));
+
             }
 
             @Override
@@ -165,6 +189,8 @@ public class MainActivity extends AppCompatActivity {
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 String outputText = getLastUpdate(dataSnapshot, "hum");
                 humidityText.setText(outputText);
+                if(isHumidity)
+                    sensorText.setText(getLatestUpdate(dataSnapshot, "hum"));
             }
 
             @Override
@@ -203,6 +229,88 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void setUpUuIdSpinner() {
+
+        sensorPicker.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+
+                if(parent.getSelectedItem().toString().equals("00002A1C-0000-1000-8000-00805F9B34FB"))
+                {
+                    Log.d("SpinnerText", "onItemSelected: "+parent.getSelectedItem().toString());
+                    isTemperature=true;
+                    isHumidity=false;
+                    tempSubtree.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            sensorText.setText(getLatestUpdate(dataSnapshot, "temp"));
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+                }
+                else
+                {
+                    isTemperature=false;
+                    isHumidity=true;
+                    humSubtree.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            sensorText.setText(getLatestUpdate(dataSnapshot, "hum"));
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+    }
+
+    private void setupLocationSpinner() {
+
+        locationPicker.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.i("PICKER", "ITEM SELECTED: " + parent.getSelectedItem().toString());
+                location = parent.getSelectedItem().toString();
+
+                locationSubtree.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        String outputText = getAvarageForLocation(dataSnapshot);
+                        averageText.setText(outputText);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError error) {
+                        // Failed to read value
+                        Log.w("Database", "Failed to read value.", error.toException());
+                    }
+                });
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+    }
+
+
+
     /**
      * This method initializes the UI.
      */
@@ -211,6 +319,8 @@ public class MainActivity extends AppCompatActivity {
         humidityText = findViewById(R.id.humidityText);
         averageText = findViewById(R.id.averageText);
         locationPicker = findViewById(R.id.locationPicker);
+        sensorPicker=findViewById(R.id.SensorPicker);
+        sensorText=findViewById(R.id.sensorText);
     }
 
     /**
@@ -229,6 +339,17 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, locations);
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         locationPicker.setAdapter(spinnerAdapter);
+    }
+
+
+    private void updateUuidPicker(DataSnapshot uuIdNode) {
+        List<String> uuId = new ArrayList<String>();
+        for (DataSnapshot singleUuId : uuIdNode.getChildren()) {
+            uuId.add(singleUuId.getKey());
+        }
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, uuId);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sensorPicker.setAdapter(spinnerAdapter);
     }
 
     /**
@@ -321,6 +442,52 @@ public class MainActivity extends AppCompatActivity {
 
         return output;
     }
+
+
+    /**
+     * This method returns the string that should be output to the gui.
+     * This output includes the newest values only from any of the two sensors.
+     *
+     * @param parentNode
+     * @param sensorType
+     * @return
+     */
+
+    private String getLatestUpdate(DataSnapshot parentNode, String sensorType) {
+        // Build output depending on sensor
+        String output = sensorType.equals("temp") ? "Temperature:" : "Humidity:";
+        output = output + System.lineSeparator();
+        String unit = sensorType.equals("temp") ? "°C" : "%";
+
+        // Init Database data
+        List<DataSnapshot> children = new ArrayList<DataSnapshot>();
+        DataSnapshot currentNode = null;
+
+        // Order all Nodes correctly
+        for (DataSnapshot childNode : parentNode.getChildren()) {
+            children.add(0, childNode);
+        }
+
+        // Iterate children from newest to oldest
+        for (DataSnapshot childNode : children) {
+            // First node is always the newest
+            if (currentNode == null) {
+                currentNode = childNode;
+                continue;
+            }
+        }
+
+        // Build string for output depending on the findings
+        if (currentNode != null) {
+            output = output + "Last update at " + timeFormat.format(new Date(Long.parseLong(currentNode.getKey()))) + " with " + currentNode.getValue(Float.class) + unit + System.lineSeparator();
+        } else {
+            return output + "No temperature data";
+        }
+
+        return output;
+    }
+
+
 
     /**
      * This method calculates the average temperature of a given date at a given location.
@@ -507,4 +674,90 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     };
+
+
+
+    //code for runtime permission
+    @TargetApi(Build.VERSION_CODES.M)
+    private void permissionCheck() {
+        List<String> permissionsNeeded = new ArrayList<String>();
+
+        final List<String> permissionsList = new ArrayList<String>();
+        if (!addPermission(permissionsList, Manifest.permission.ACCESS_FINE_LOCATION))
+            permissionsNeeded.add("Show Location");
+
+        if (permissionsList.size() > 0) {
+            if (permissionsNeeded.size() > 0) {
+
+                // Need Rationale
+                String message = "App need access to " + permissionsNeeded.get(0);
+
+                for (int i = 1; i < permissionsNeeded.size(); i++)
+                    message = message + ", " + permissionsNeeded.get(i);
+
+                showMessageOKCancel(message,
+                        new DialogInterface.OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                requestPermissions(permissionsList.toArray(new String[permissionsList.size()]),
+                                        REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS);
+                            }
+                        });
+                return;
+            }
+            requestPermissions(permissionsList.toArray(new String[permissionsList.size()]),
+                    REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS);
+            return;
+        }
+    }
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private boolean addPermission(List<String> permissionsList, String permission) {
+
+        if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
+            permissionsList.add(permission);
+            // Check for Rationale Option
+            if (!shouldShowRequestPermissionRationale(permission))
+                return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS: {
+                Map<String, Integer> perms = new HashMap<String, Integer>();
+                // Initial
+                perms.put(Manifest.permission.ACCESS_FINE_LOCATION, PackageManager.PERMISSION_GRANTED);
+
+
+                // Fill with results
+                for (int i = 0; i < permissions.length; i++)
+                    perms.put(permissions[i], grantResults[i]);
+
+                // Check for ACCESS_FINE_LOCATION
+                if (perms.get(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                    Toast.makeText(this, "One or More Permissions are DENIED ", Toast.LENGTH_SHORT)
+                            .show();
+
+
+                }
+            }
+            break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
 }
